1ab378172fcbe28e0735052db6d159a5b8f6e20d
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-03-12 21:00:05 +0900

    add tableview of registered information and write button to WRITE_JSON state

--- src/main/java/capslock/kiddy_register/main/WriteController.java
+++ src/main/java/capslock/kiddy_register/main/WriteController.java
@@ -1,16 +1,63 @@
 package capslock.kiddy_register.main;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import methg.commonlib.trivial_logger.Logger;
 
 public class WriteController extends ChildController{
+
+    @FXML private TableView<Field> tableView;
+    @FXML private TableColumn keyCol;
+    @FXML private TableColumn valueCol;
+    private ObservableList<Field> data;
+
+    public static class Field{
+        private final SimpleStringProperty key;
+        private final SimpleStringProperty value;
+
+        private Field(String key, String value){
+            this.key = new SimpleStringProperty(key);
+            this.value = new SimpleStringProperty(value);
+        }
+
+        public String getKey() {
+            return key.get();
+        }
+
+        public String getValue() {
+            return value.get();
+        }
+    }
 
 
     @Override
     public final void init() {
         Logger.INST.debug("WriteController#init called");
 
-        MainHandler.INST.writeToJSON();
+        keyCol.setCellValueFactory(new PropertyValueFactory<>("key"));
+        valueCol.setCellValueFactory(new PropertyValueFactory<>("value"));
 
+        data = FXCollections.observableArrayList(
+                new Field("ゲームの実行ファイル", MainHandler.INST.getExe().toString()),
+                new Field("ゲーム名", MainHandler.INST.getName()),
+                new Field("ゲームの説明", MainHandler.INST.getDesc()),
+                new Field("パネル画像", MainHandler.INST.getPanel().toString()),
+                new Field("紹介画像", MainHandler.INST.getImageList().toString()),
+                new Field("紹介動画", MainHandler.INST.getMovieList().toString()),
+                new Field("ゲームID", Integer.toString(MainHandler.INST.getID()))
+        );
+
+        tableView.setItems(data);
+    }
+
+    @FXML private void onWritePushed(ActionEvent event){
+        MainHandler.INST.writeToJSON();
         MainHandler.INST.cacheGameRoot();
     }
 }
