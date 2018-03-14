739e6965fb7c932ca270ed5fa05fdd6596fd6da9
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-03-14 10:35:13 +0900

    Movie scene preview movies as snapshots

--- src/main/java/capslock/kiddy_register/main/MovieController.java
+++ src/main/java/capslock/kiddy_register/main/MovieController.java
@@ -1,10 +1,19 @@
 package capslock.kiddy_register.main;
 
 import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaException;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
 import methg.commonlib.file_checker.FileChecker;
 import methg.commonlib.trivial_logger.Logger;
 
@@ -13,6 +22,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.FutureTask;
 
 public class MovieController extends ChildController{
 
@@ -41,12 +51,15 @@ public class MovieController extends ChildController{
             }
         }
 
+        movieList.forEach(this::requestDisplay);
+
         MainHandler.INST.setMovieList(movieList);
 
         event.setDropCompleted(true);
         event.consume();
 
         parentController.enableNextButton();
+        Logger.INST.debug("drop quit");
     }
 
     @FXML private void onDragOver(DragEvent event) {
@@ -70,5 +83,57 @@ public class MovieController extends ChildController{
             }
         }
         event.consume();
+    }
+
+    final void requestDisplay(Path path){
+        final Media media = new Media(path.toUri().toString());
+
+        try {
+            MediaException exception = media.getError();
+            if(exception != null)throw exception;
+        }catch (Exception ex){
+            Logger.INST.logException(ex);
+        }
+
+        Logger.INST.debug("media created");
+
+        media.setOnError(() -> Logger.INST.critical("MediaError"));
+
+
+        if(media == null)throw new NullPointerException();
+
+        final MediaPlayer player = new MediaPlayer(media);
+
+        System.err.println(player.getTotalDuration());
+        player.setOnError(() -> Logger.INST.critical("onError"));
+        player.setOnStopped(() -> Logger.INST.critical("onStopped"));
+        player.setOnHalted(() -> Logger.INST.critical("onHalted"));
+
+        WritableImage snapshot = new WritableImage(100, 100);
+
+        player.setOnError(() -> Logger.INST.critical("play error"));
+        //player.setAutoPlay(true);
+        player.setCycleCount(1);
+
+        if(player == null)throw new NullPointerException();
+
+        final MediaView mediaView = new MediaView(player);
+        mediaView.setPreserveRatio(true);
+        //mediaView.setFitWidth(100);
+        final Scene scene = new Scene (new Group(mediaView), 100, 100);
+        //player.play();
+        //player.pause();
+
+        if(mediaView == null)throw new NullPointerException();
+
+        // player.setOnPlaying(futureTask);
+
+        player.setOnReady(() -> {
+            Logger.INST.debug("OnReady entry");
+            final ImageView view = new ImageView(scene.snapshot(snapshot));
+            flowPane.getChildren().add(view);
+        });
+
+        player.play();
     }
 }
