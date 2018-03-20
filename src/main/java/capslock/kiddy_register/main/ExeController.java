95e02273240ce7e47ebdded18120697232e17cba
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-03-21 00:31:48 +0900

    Exe scene is safer.

--- src/main/java/capslock/kiddy_register/main/ExeController.java
+++ src/main/java/capslock/kiddy_register/main/ExeController.java
@@ -20,27 +20,42 @@ import javafx.scene.control.Label;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
-import methg.commonlib.file_checker.FileChecker;
+import javafx.scene.paint.Paint;
 import methg.commonlib.trivial_logger.Logger;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 public class ExeController extends ChildController{
-    @FXML
-    private Label pathLabel;
+    @FXML private Label pathLabel;
+    @FXML private Label warnLabel;
 
     @Override
     public final void init() {
         Logger.INST.debug("Exe init called");
 
         final Path exe = MainHandler.INST.getExe();
-        if (exe == null) {
-            pathLabel.setText("まだ指定されていません.");
-        } else {
-            pathLabel.setText(exe.toString());
+
+        if (exe == null)return;
+
+        pathLabel.setText(exe.toString());
+
+        if(Files.notExists(exe)) {
+            warnLabel.setText("ファイルが見つかりません.指定し直して下さい.");
+            warnLabel.setTextFill(Paint.valueOf("red"));
+            return;
+        }
+
+        if(validateExe(exe, false)){
+            warnLabel.setText("登録内容は正常です.");
+            warnLabel.setTextFill(Paint.valueOf("green"));
             parentController.enableNextButton();
         }
     }
@@ -55,6 +70,8 @@ public class ExeController extends ChildController{
         event.setDropCompleted(true);
         event.consume();
 
+        warnLabel.setText("登録内容は正常です.");
+        warnLabel.setTextFill(Paint.valueOf("green"));
         parentController.enableNextButton();
     }
 
@@ -62,19 +79,83 @@ public class ExeController extends ChildController{
         final Dragboard board = event.getDragboard();
         checkContent: if(board.hasFiles()){
             final List<File> fileList = board.getFiles();
-            if(fileList.size() != 1)break checkContent;
+            if(fileList.size() != 1){
+                warnLabel.setText("ゲームの実行ファイルは1つのみ登録できます.");
+                warnLabel.setTextFill(Paint.valueOf("red"));
+                break checkContent;
+            }
 
-            final Path exePath = fileList.get(0).toPath();
-
-            final Optional<Path> exe = new FileChecker(exePath)
-                    .onCannotRead(dummy -> true)
-                    .onCannotWrite(dummy -> true)
-                    .onCanExec(dummy -> true)
-                    .check();
-
-            if(exe.isPresent() && exe.get().startsWith(MainHandler.INST.getGameRootDir()))
+            if(validateExe(fileList.get(0).toPath(), true)) {
+                warnLabel.setText("登録可能です.");
+                warnLabel.setTextFill(Paint.valueOf("green"));
                 event.acceptTransferModes(TransferMode.LINK);
+            }
         }
         event.consume();
+    }
+
+    @FXML private void onDragExited(DragEvent event){
+        final Path exe = MainHandler.INST.getExe();
+        if(exe == null){
+            warnLabel.setText("まだ登録されていません.");
+            warnLabel.setTextFill(Paint.valueOf("red"));
+            return;
+        }
+
+        if(validateExe(exe, false)){
+            warnLabel.setText("登録内容は正常です.");
+            warnLabel.setTextFill(Paint.valueOf("green"));
+        }
+    }
+
+    /**
+     * ファイルがゲームの実行ファイルとしての要件を満たすかどうか, つまりランチャーで正常に起動できるかどうか検証する.
+     * <p>副作用で{@link #warnLabel}にエラー内容を表示する.</p>
+     * @param path 検証対象のファイルパス
+     * @param isBeingDragged ドラッグ&ドロップ中かどうか
+     * @return {@code true}引数がゲームの実行ファイルとしての要件を満たす. {@code false}それ以外
+     */
+    private boolean validateExe(Path path, boolean isBeingDragged) {
+        final String demonstrative = isBeingDragged ? "その" : "この";
+        if(!Files.isRegularFile(path, LinkOption.NOFOLLOW_LINKS)){
+            Logger.INST.debug("It isn't a regular file.");
+            warnLabel.setTextFill(Paint.valueOf("red"));
+            warnLabel.setText(demonstrative + "ファイルは通常ファイルではありません.");
+            return false;
+        }
+
+        if(!path.startsWith(MainHandler.INST.getGameRootDir())){
+            Logger.INST.debug("Exe file is at the outside of the root directory of the game.");
+            warnLabel.setTextFill(Paint.valueOf("red"));
+            warnLabel.setText(demonstrative + "ファイルはゲームのルートディレクトリの中にありません.");
+            return false;
+        }
+
+        if(!Files.isExecutable(path)){
+            Logger.INST.debug("Not executable.");
+            warnLabel.setTextFill(Paint.valueOf("red"));
+            warnLabel.setText(demonstrative + "ファイルには実行権限が与えられていません.");
+            return false;
+        }
+
+        //先頭の2byteを読み出して,MSDOSのEXEファイルフォーマットのマジックナンバー"MZ"と一致するか検証する.
+        try (final InputStream inputStream = Files.newInputStream(path, StandardOpenOption.READ)){
+            byte[] magicNumber = new byte[2];
+            inputStream.read(magicNumber);
+            System.err.println("[0] = " + magicNumber[0] + ", [1] = " + magicNumber[1]);
+            if(Arrays.equals(magicNumber, new byte[]{'M', 'Z'})){
+                Logger.INST.debug("Ok, this is exe format file.");
+                return true;
+            }else{
+                warnLabel.setTextFill(Paint.valueOf("red"));
+                warnLabel.setText(demonstrative + "ファイルは実行できません.");
+                return false;
+            }
+        }catch (IOException ex){
+            Logger.INST.logException(ex);
+            warnLabel.setTextFill(Paint.valueOf("yellow"));
+            warnLabel.setText(demonstrative + "ファイルは実行できない可能性があります.");
+            return true;
+        }
     }
 }
